278d814f099a1ed387ec3ad75b48248ee184b086
RainyKnight <stoltmanlucas@gmail.com>
2021-03-28 21:16:09 -0700

    Add a todo list

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ http.createServer(function (request, response) {
 
     fs.readFile('bitwarden_export_test.csv', function(err, data){
 
+        // build an array to store the data
+        // let passwords[]
+
         response.writeHead(200, { 'Content-Type': 'text/html' });
         response.write(data);
         return response.end();
@@ -17,3 +20,19 @@ http.createServer(function (request, response) {
 }).listen(port);
 
 console.log(`Console running at http://127.0.0.1:${port}`); 
+
+
+// TODO
+
+/*
+
+* make a new array clean of duplicates
+
+* create a new csv file of the new array
+
+
+
+
+
+
+*/
