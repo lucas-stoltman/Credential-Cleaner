be307d4660d7c768ea31eeae8f3e8955af7c847a
RainyKnight <stoltmanlucas@gmail.com>
2021-03-29 18:04:48 -0700

    Create an array of rows of data

--- app.js
+++ app.js
@@ -2,12 +2,15 @@
 const csv = require('csv-parser');
 const fs = require('fs'); // filesystem
 
+let data = [];
+
 fs.createReadStream('bitwarden_export_test.csv')
     .pipe(csv())
     .on('data', (row) => {
-        console.log(row);
+        data.push(row);
     })
     .on('end', () => {
+        console.table(data);
         console.log('CSV file successfully processed');
     });
 
