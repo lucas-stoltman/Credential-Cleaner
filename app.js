6848b6bc3c3a725f465012e6a58b39d6c41e89cd
RainyKnight <stoltmanlucas@gmail.com>
2021-04-01 22:17:17 -0700

    Add pseudocode for array logic

--- app.js
+++ app.js
@@ -3,27 +3,29 @@ const csv = require('csv-parser');
 const fs = require('fs'); // filesystem
 
 let data = [];
+let rowString;
 
 fs.createReadStream('bitwarden_export_test.csv')
     .pipe(csv())
     .on('data', (row) => {
-        data.push(row);
+        console.log(row); 
+        data.push(rowString);
     })
 
-    // sort the array first
+// sort the array first for faster runtime
+// if not, build new array and compare for duplicates when adding
 
 
-    for (let i = 0; i < data.length; i++) {
-        if (data[i] == data[i+1]) {
-            data.splice(i, 1);
-        }
+for (let i = 0; i < data.length; i++) {
+    if (data[i] == data[i + 1]) {
+        data.splice(i, 1);
     }
+}
 
-    .on('end', () => {
-        console.log(data);
-        console.log('CSV file successfully processed');
-    });
-
+// console.log(`Slot 0: ${data[0]}`);
+// console.log(`Slot 1: ${data[1]}`);
+// console.log(data[0] > data[1]);
+// console.log('CSV file successfully processed');
 // TODO
 
 /*
