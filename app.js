82c318a20723cdde5a33cae95fa7e203ef47c445
RainyKnight <stoltmanlucas@gmail.com>
2021-04-04 15:16:46 -0700

    Add a method to write a new csv file

--- app.js
+++ app.js
@@ -1,21 +1,59 @@
 // Adding modules
 const csv = require('csv-parser');
+const fastcsv = require('fast-csv');
 const fs = require('fs'); // filesystem
-const results = [];
+let results = [];
+
 
 fs.createReadStream('data.csv')
     .pipe(csv())
     .on('data', (data) => results.push(data))
     .on('end', () => {
+
+        // post the original
         console.log('\nBefore\n');
-        console.table(results);
+        // console.table(results);
+
+        // remove duplicates
+        clean(results);
+
+        // post the cleaned version
+        console.log('\nAfter\n');
+        // console.table();
+
+        // create new csv file
+        const ws = fs.createWriteStream("out.csv");
+        fastcsv
+            .write(results, { headers: true })
+            .pipe(ws);
+
 
     });
 
-function clean(results) {
+
+function clean(array) {
+
+    let arrayCopy = [];
+
+    // iterate through original array
+    for (i = 0; i < array.length; i++) {
+
+        let numDuplicates = 0;
+
+        // compare current value with every value in the copy
+        for (i = 0; i < array.length; i++) {
+            if (array[i] == arrayCopy[i]) {
+                arrayCopy.slice()
+            }
+        }
+    }
+
+    console.table(arrayCopy);
 
 }
 
+
+// TODO List
 /*
 
 * make a new array clean of duplicates
